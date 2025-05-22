0d44f23bb349db0673c37d703b81bac347af53c1
Данил Митясов <mrstickmananimation@gmail.com>
2025-05-22 21:54:03 +0700

    Добавьте файлы проекта.

---
+++ kursfordim/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace kursfordim
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            dataGridViewMachines = new DataGridView();
+            dataGridViewOils = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            btnCalculate = new Button();
+            label3 = new Label();
+            txtTotalPlanCost = new TextBox();
+            label4 = new Label();
+            txtTotalActualCost = new TextBox();
+            label5 = new Label();
+            txtMinCost = new TextBox();
+            label6 = new Label();
+            txtMaxCost = new TextBox();
+            label7 = new Label();
+            listBoxOilByStockCost = new ListBox();
+            btnRandomFill = new Button();
+            btnImport = new Button();
+            btnExport = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMachines).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOils).BeginInit();
+            SuspendLayout();
+
+            // dataGridViewMachines
+            dataGridViewMachines.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewMachines.Location = new Point(12, 36);
+            dataGridViewMachines.Name = "dataGridViewMachines";
+            dataGridViewMachines.Size = new Size(600, 150);
+            dataGridViewMachines.TabIndex = 0;
+
+            // dataGridViewOils
+            dataGridViewOils.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewOils.Location = new Point(12, 225);
+            dataGridViewOils.Name = "dataGridViewOils";
+            dataGridViewOils.Size = new Size(600, 150);
+            dataGridViewOils.TabIndex = 1;
+
+            // label1
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 13);
+            label1.TabIndex = 2;
+            label1.Text = "Виды станков:";
+
+            // label2
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 209);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 13);
+            label2.TabIndex = 3;
+            label2.Text = "Марки масел (ГСМ):";
+
+            // btnCalculate
+            btnCalculate.Location = new Point(618, 36);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(150, 40);
+            btnCalculate.TabIndex = 4;
+            btnCalculate.Text = "Рассчитать";
+            btnCalculate.UseVisualStyleBackColor = true;
+            btnCalculate.Click += btnCalculate_Click;
+
+            // label3
+            label3.AutoSize = true;
+            label3.Location = new Point(618, 100);
+            label3.Name = "label3";
+            label3.Size = new Size(150, 13);
+            label3.TabIndex = 5;
+            label3.Text = "Общая плановая стоимость:";
+
+            // txtTotalPlanCost
+            txtTotalPlanCost.Location = new Point(618, 116);
+            txtTotalPlanCost.Name = "txtTotalPlanCost";
+            txtTotalPlanCost.ReadOnly = true;
+            txtTotalPlanCost.Size = new Size(150, 20);
+            txtTotalPlanCost.TabIndex = 6;
+
+            // label4
+            label4.AutoSize = true;
+            label4.Location = new Point(618, 150);
+            label4.Name = "label4";
+            label4.Size = new Size(154, 13);
+            label4.TabIndex = 7;
+            label4.Text = "Общая фактическая стоимость:";
+
+            // txtTotalActualCost
+            txtTotalActualCost.Location = new Point(618, 166);
+            txtTotalActualCost.Name = "txtTotalActualCost";
+            txtTotalActualCost.ReadOnly = true;
+            txtTotalActualCost.Size = new Size(150, 20);
+            txtTotalActualCost.TabIndex = 8;
+
+            // label5
+            label5.AutoSize = true;
+            label5.Location = new Point(618, 200);
+            label5.Name = "label5";
+            label5.Size = new Size(113, 13);
+            label5.TabIndex = 9;
+            label5.Text = "Минимальная стоимость:";
+
+            // txtMinCost
+            txtMinCost.Location = new Point(618, 216);
+            txtMinCost.Name = "txtMinCost";
+            txtMinCost.ReadOnly = true;
+            txtMinCost.Size = new Size(150, 20);
+            txtMinCost.TabIndex = 10;
+
+            // label6
+            label6.AutoSize = true;
+            label6.Location = new Point(618, 250);
+            label6.Name = "label6";
+            label6.Size = new Size(119, 13);
+            label6.TabIndex = 11;
+            label6.Text = "Максимальная стоимость:";
+
+            // txtMaxCost
+            txtMaxCost.Location = new Point(618, 266);
+            txtMaxCost.Name = "txtMaxCost";
+            txtMaxCost.ReadOnly = true;
+            txtMaxCost.Size = new Size(150, 20);
+            txtMaxCost.TabIndex = 12;
+
+            // label7
+            label7.AutoSize = true;
+            label7.Location = new Point(618, 300);
+            label7.Name = "label7";
+            label7.Size = new Size(161, 13);
+            label7.TabIndex = 13;
+            label7.Text = "Масла по стоимости остатков:";
+
+            // listBoxOilByStockCost
+            listBoxOilByStockCost.FormattingEnabled = true;
+            listBoxOilByStockCost.Location = new Point(618, 316);
+            listBoxOilByStockCost.Name = "listBoxOilByStockCost";
+            listBoxOilByStockCost.Size = new Size(150, 95);
+            listBoxOilByStockCost.TabIndex = 14;
+
+            // btnRandomFill
+            btnRandomFill.Location = new Point(12, 381);
+            btnRandomFill.Name = "btnRandomFill";
+            btnRandomFill.Size = new Size(150, 30);
+            btnRandomFill.TabIndex = 15;
+            btnRandomFill.Text = "Случайное заполнение";
+            btnRandomFill.UseVisualStyleBackColor = true;
+            btnRandomFill.Click += btnRandomFill_Click;
+
+            // btnImport
+            btnImport.Location = new Point(168, 381);
+            btnImport.Name = "btnImport";
+            btnImport.Size = new Size(150, 30);
+            btnImport.TabIndex = 16;
+            btnImport.Text = "Импорт из файла";
+            btnImport.UseVisualStyleBackColor = true;
+            btnImport.Click += btnImport_Click;
+
+            // btnExport
+            btnExport.Location = new Point(324, 381);
+            btnExport.Name = "btnExport";
+            btnExport.Size = new Size(150, 30);
+            btnExport.TabIndex = 17;
+            btnExport.Text = "Экспорт в файл";
+            btnExport.UseVisualStyleBackColor = true;
+            btnExport.Click += btnExport_Click;
+
+            // Form1
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 421);
+            Controls.Add(btnExport);
+            Controls.Add(btnImport);
+            Controls.Add(btnRandomFill);
+            Controls.Add(listBoxOilByStockCost);
+            Controls.Add(label7);
+            Controls.Add(txtMaxCost);
+            Controls.Add(label6);
+            Controls.Add(txtMinCost);
+            Controls.Add(label5);
+            Controls.Add(txtTotalActualCost);
+            Controls.Add(label4);
+            Controls.Add(txtTotalPlanCost);
+            Controls.Add(label3);
+            Controls.Add(btnCalculate);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridViewOils);
+            Controls.Add(dataGridViewMachines);
+            Name = "Form1";
+            Text = "Учет технических масел для станков";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewMachines).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewOils).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private DataGridView dataGridViewMachines;
+        private DataGridView dataGridViewOils;
+        private Label label1;
+        private Label label2;
+        private Button btnCalculate;
+        private Label label3;
+        private TextBox txtTotalPlanCost;
+        private Label label4;
+        private TextBox txtTotalActualCost;
+        private Label label5;
+        private TextBox txtMinCost;
+        private Label label6;
+        private TextBox txtMaxCost;
+        private Label label7;
+        private ListBox listBoxOilByStockCost;
+        private Button btnRandomFill;
+        private Button btnImport;
+        private Button btnExport;
+    }
+}
---
+++ kursfordim/Form1.cs
@@ -0,0 +1,250 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace kursfordim
+{
+    public partial class Form1 : Form
+    {
+        private DataTable machinesTable;
+        private DataTable oilsTable;
+        private Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataTables();
+        }
+
+        private void InitializeDataTables()
+        {
+            // Таблица станков
+            machinesTable = new DataTable();
+            machinesTable.Columns.Add("Тип станка", typeof(string));
+            machinesTable.Columns.Add("Количество", typeof(int));
+            machinesTable.Columns.Add("Марка масла", typeof(string));
+            machinesTable.Columns.Add("Плановый расход", typeof(decimal));
+            machinesTable.Columns.Add("Фактический расход", typeof(decimal));
+
+            dataGridViewMachines.DataSource = machinesTable;
+
+            // Таблица масел
+            oilsTable = new DataTable();
+            oilsTable.Columns.Add("Марка масла", typeof(string));
+            oilsTable.Columns.Add("Количество на складе", typeof(decimal));
+            oilsTable.Columns.Add("Стоимость за единицу", typeof(decimal));
+
+            dataGridViewOils.DataSource = oilsTable;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            CalculateCosts();
+            CalculateMinMaxCosts();
+            DisplayOilsByStockCost();
+        }
+
+        private void CalculateCosts()
+        {
+            decimal totalPlanCost = 0;
+            decimal totalActualCost = 0;
+
+            foreach (DataRow machineRow in machinesTable.Rows)
+            {
+                string oilBrand = machineRow["Марка масла"].ToString();
+                decimal planConsumption = Convert.ToDecimal(machineRow["Плановый расход"]);
+                decimal actualConsumption = Convert.ToDecimal(machineRow["Фактический расход"]);
+                int machineCount = Convert.ToInt32(machineRow["Количество"]);
+
+                DataRow[] oilRows = oilsTable.Select($"[Марка масла] = '{oilBrand}'");
+                if (oilRows.Length > 0)
+                {
+                    decimal oilPrice = Convert.ToDecimal(oilRows[0]["Стоимость за единицу"]);
+                    totalPlanCost += planConsumption * oilPrice * machineCount;
+                    totalActualCost += actualConsumption * oilPrice * machineCount;
+                }
+            }
+
+            txtTotalPlanCost.Text = totalPlanCost.ToString("N2");
+            txtTotalActualCost.Text = totalActualCost.ToString("N2");
+        }
+
+        private void CalculateMinMaxCosts()
+        {
+            List<decimal> costs = new List<decimal>();
+
+            foreach (DataRow machineRow in machinesTable.Rows)
+            {
+                string oilBrand = machineRow["Марка масла"].ToString();
+                decimal actualConsumption = Convert.ToDecimal(machineRow["Фактический расход"]);
+                int machineCount = Convert.ToInt32(machineRow["Количество"]);
+
+                DataRow[] oilRows = oilsTable.Select($"[Марка масла] = '{oilBrand}'");
+                if (oilRows.Length > 0)
+                {
+                    decimal oilPrice = Convert.ToDecimal(oilRows[0]["Стоимость за единицу"]);
+                    decimal cost = actualConsumption * oilPrice * machineCount;
+                    costs.Add(cost);
+                }
+            }
+
+            if (costs.Count > 0)
+            {
+                txtMinCost.Text = costs.Min().ToString("N2");
+                txtMaxCost.Text = costs.Max().ToString("N2");
+            }
+        }
+
+        private void DisplayOilsByStockCost()
+        {
+            listBoxOilByStockCost.Items.Clear();
+
+            var oils = oilsTable.AsEnumerable()
+                .OrderBy(row => Convert.ToDecimal(row["Количество на складе"]) *
+                               Convert.ToDecimal(row["Стоимость за единицу"]))
+                .Select(row => $"{row["Марка масла"]} - {Convert.ToDecimal(row["Количество на складе"]) *
+                              Convert.ToDecimal(row["Стоимость за единицу"]):N2}");
+
+            foreach (var oil in oils)
+            {
+                listBoxOilByStockCost.Items.Add(oil);
+            }
+        }
+
+        private void btnRandomFill_Click(object sender, EventArgs e)
+        {
+            // Очищаем таблицы
+            machinesTable.Rows.Clear();
+            oilsTable.Rows.Clear();
+
+            // Случайные марки масел
+            string[] oilBrands = { "Марка A", "Марка B", "Марка C", "Марка D", "Марка E" };
+            string[] machineTypes = { "Токарный", "Фрезерный", "Сверлильный", "Шлифовальный", "Фрезерно-токарный" };
+
+            // Заполняем масла
+            foreach (string brand in oilBrands)
+            {
+                oilsTable.Rows.Add(
+                    brand,
+                    Convert.ToDecimal(Math.Round(random.Next(50, 200) + random.NextDouble(), 2)), // Количество
+                    Convert.ToDecimal(Math.Round(random.Next(500, 2000) + random.NextDouble(), 2)) // Цена
+                );
+            }
+
+            // Заполняем станки
+            for (int i = 0; i < 5; i++)
+            {
+                machinesTable.Rows.Add(
+                    machineTypes[i],
+                    random.Next(1, 10), // Количество станков
+                    oilBrands[random.Next(oilBrands.Length)], // Марка масла
+                    Math.Round(random.Next(1, 20) + random.NextDouble(), 2), // Плановый расход
+                    Math.Round(random.Next(1, 20) + random.NextDouble(), 2) // Фактический расход
+                );
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(openFileDialog.FileName);
+                    if (lines.Length < 2) return;
+
+                    // Очищаем таблицы
+                    machinesTable.Rows.Clear();
+                    oilsTable.Rows.Clear();
+
+                    // Читаем масла (первая строка - заголовок)
+                    int i = 1;
+                    while (i < lines.Length && !string.IsNullOrWhiteSpace(lines[i]))
+                    {
+                        string[] parts = lines[i].Split('|');
+                        if (parts.Length >= 3)
+                        {
+                            oilsTable.Rows.Add(parts[0], decimal.Parse(parts[1]), decimal.Parse(parts[2]));
+                        }
+                        i++;
+                    }
+
+                    // Пропускаем пустую строку
+                    i++;
+
+                    // Читаем станки (строка после пустой)
+                    while (i < lines.Length)
+                    {
+                        string[] parts = lines[i].Split('|');
+                        if (parts.Length >= 5)
+                        {
+                            machinesTable.Rows.Add(
+                                parts[0],
+                                int.Parse(parts[1]),
+                                parts[2],
+                                decimal.Parse(parts[3]),
+                                decimal.Parse(parts[4]));
+                        }
+                        i++;
+                    }
+
+                    MessageBox.Show("Данные успешно загружены", "Импорт", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при импорте: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    List<string> lines = new List<string>();
+
+                    // Экспорт масел
+                    lines.Add("Марка масла|Количество|Цена");
+                    foreach (DataRow row in oilsTable.Rows)
+                    {
+                        lines.Add($"{row["Марка масла"]}|{row["Количество на складе"]}|{row["Стоимость за единицу"]}");
+                    }
+
+                    // Пустая строка как разделитель
+                    lines.Add("");
+
+                    // Экспорт станков
+                    lines.Add("Тип станка|Количество|Марка масла|Плановый расход|Фактический расход");
+                    foreach (DataRow row in machinesTable.Rows)
+                    {
+                        lines.Add($"{row["Тип станка"]}|{row["Количество"]}|{row["Марка масла"]}|" +
+                                $"{row["Плановый расход"]}|{row["Фактический расход"]}");
+                    }
+
+                    File.WriteAllLines(saveFileDialog.FileName, lines);
+                    MessageBox.Show("Данные успешно экспортированы", "Экспорт", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при экспорте: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Можно добавить начальную загрузку данных, если нужно
+        }
+    }
+}
