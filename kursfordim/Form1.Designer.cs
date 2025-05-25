141336410fe99cbb294b00b143357770414ef276
Данил Митясов <mrstickmananimation@gmail.com>
2025-05-25 13:37:50 +0700

    3

--- kursfordim/Form1.Designer.cs
+++ kursfordim/Form1.Designer.cs
@@ -37,155 +37,236 @@ namespace kursfordim
             btnRandomFill = new Button();
             btnImport = new Button();
             btnExport = new Button();
+            menuStrip1 = new MenuStrip();
+            импортToolStripMenuItem = new ToolStripMenuItem();
+            экспортToolStripMenuItem = new ToolStripMenuItem();
+            рассчитатьToolStripMenuItem = new ToolStripMenuItem();
+            случайноеЗаполнениеToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)dataGridViewMachines).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridViewOils).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
-
+            // 
             // dataGridViewMachines
+            // 
             dataGridViewMachines.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewMachines.Location = new Point(12, 36);
+            dataGridViewMachines.Location = new Point(14, 42);
+            dataGridViewMachines.Margin = new Padding(4, 3, 4, 3);
             dataGridViewMachines.Name = "dataGridViewMachines";
-            dataGridViewMachines.Size = new Size(600, 150);
+            dataGridViewMachines.Size = new Size(700, 173);
             dataGridViewMachines.TabIndex = 0;
-
+            // 
             // dataGridViewOils
+            // 
             dataGridViewOils.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewOils.Location = new Point(12, 225);
+            dataGridViewOils.Location = new Point(14, 260);
+            dataGridViewOils.Margin = new Padding(4, 3, 4, 3);
             dataGridViewOils.Name = "dataGridViewOils";
-            dataGridViewOils.Size = new Size(600, 150);
+            dataGridViewOils.Size = new Size(700, 173);
             dataGridViewOils.TabIndex = 1;
-
+            // 
             // label1
+            // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 20);
+            label1.Location = new Point(14, 23);
+            label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(82, 13);
+            label1.Size = new Size(85, 15);
             label1.TabIndex = 2;
             label1.Text = "Виды станков:";
-
+            // 
             // label2
+            // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 209);
+            label2.Location = new Point(14, 241);
+            label2.Margin = new Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new Size(100, 13);
+            label2.Size = new Size(120, 15);
             label2.TabIndex = 3;
             label2.Text = "Марки масел (ГСМ):";
-
+            // 
             // btnCalculate
-            btnCalculate.Location = new Point(618, 36);
+            // 
+            btnCalculate.Location = new Point(721, 42);
+            btnCalculate.Margin = new Padding(4, 3, 4, 3);
             btnCalculate.Name = "btnCalculate";
-            btnCalculate.Size = new Size(150, 40);
+            btnCalculate.Size = new Size(175, 46);
             btnCalculate.TabIndex = 4;
             btnCalculate.Text = "Рассчитать";
             btnCalculate.UseVisualStyleBackColor = true;
             btnCalculate.Click += btnCalculate_Click;
-
+            // 
             // label3
+            // 
             label3.AutoSize = true;
-            label3.Location = new Point(618, 100);
+            label3.Location = new Point(721, 115);
+            label3.Margin = new Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new Size(150, 13);
+            label3.Size = new Size(165, 15);
             label3.TabIndex = 5;
             label3.Text = "Общая плановая стоимость:";
-
+            // 
             // txtTotalPlanCost
-            txtTotalPlanCost.Location = new Point(618, 116);
+            // 
+            txtTotalPlanCost.Location = new Point(721, 134);
+            txtTotalPlanCost.Margin = new Padding(4, 3, 4, 3);
             txtTotalPlanCost.Name = "txtTotalPlanCost";
             txtTotalPlanCost.ReadOnly = true;
-            txtTotalPlanCost.Size = new Size(150, 20);
+            txtTotalPlanCost.Size = new Size(174, 23);
             txtTotalPlanCost.TabIndex = 6;
-
+            // 
             // label4
+            // 
             label4.AutoSize = true;
-            label4.Location = new Point(618, 150);
+            label4.Location = new Point(721, 173);
+            label4.Margin = new Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new Size(154, 13);
+            label4.Size = new Size(183, 15);
             label4.TabIndex = 7;
             label4.Text = "Общая фактическая стоимость:";
-
+            // 
             // txtTotalActualCost
-            txtTotalActualCost.Location = new Point(618, 166);
+            // 
+            txtTotalActualCost.Location = new Point(721, 192);
+            txtTotalActualCost.Margin = new Padding(4, 3, 4, 3);
             txtTotalActualCost.Name = "txtTotalActualCost";
             txtTotalActualCost.ReadOnly = true;
-            txtTotalActualCost.Size = new Size(150, 20);
+            txtTotalActualCost.Size = new Size(174, 23);
             txtTotalActualCost.TabIndex = 8;
-
+            // 
             // label5
+            // 
             label5.AutoSize = true;
-            label5.Location = new Point(618, 200);
+            label5.Location = new Point(721, 231);
+            label5.Margin = new Padding(4, 0, 4, 0);
             label5.Name = "label5";
-            label5.Size = new Size(113, 13);
+            label5.Size = new Size(150, 15);
             label5.TabIndex = 9;
             label5.Text = "Минимальная стоимость:";
-
+            // 
             // txtMinCost
-            txtMinCost.Location = new Point(618, 216);
+            // 
+            txtMinCost.Location = new Point(721, 249);
+            txtMinCost.Margin = new Padding(4, 3, 4, 3);
             txtMinCost.Name = "txtMinCost";
             txtMinCost.ReadOnly = true;
-            txtMinCost.Size = new Size(150, 20);
+            txtMinCost.Size = new Size(174, 23);
             txtMinCost.TabIndex = 10;
-
+            // 
             // label6
+            // 
             label6.AutoSize = true;
-            label6.Location = new Point(618, 250);
+            label6.Location = new Point(721, 288);
+            label6.Margin = new Padding(4, 0, 4, 0);
             label6.Name = "label6";
-            label6.Size = new Size(119, 13);
+            label6.Size = new Size(154, 15);
             label6.TabIndex = 11;
             label6.Text = "Максимальная стоимость:";
-
+            // 
             // txtMaxCost
-            txtMaxCost.Location = new Point(618, 266);
+            // 
+            txtMaxCost.Location = new Point(721, 307);
+            txtMaxCost.Margin = new Padding(4, 3, 4, 3);
             txtMaxCost.Name = "txtMaxCost";
             txtMaxCost.ReadOnly = true;
-            txtMaxCost.Size = new Size(150, 20);
+            txtMaxCost.Size = new Size(174, 23);
             txtMaxCost.TabIndex = 12;
-
+            // 
             // label7
+            // 
             label7.AutoSize = true;
-            label7.Location = new Point(618, 300);
+            label7.Location = new Point(721, 346);
+            label7.Margin = new Padding(4, 0, 4, 0);
             label7.Name = "label7";
-            label7.Size = new Size(161, 13);
+            label7.Size = new Size(176, 15);
             label7.TabIndex = 13;
             label7.Text = "Масла по стоимости остатков:";
-
+            // 
             // listBoxOilByStockCost
+            // 
             listBoxOilByStockCost.FormattingEnabled = true;
-            listBoxOilByStockCost.Location = new Point(618, 316);
+            listBoxOilByStockCost.ItemHeight = 15;
+            listBoxOilByStockCost.Location = new Point(721, 365);
+            listBoxOilByStockCost.Margin = new Padding(4, 3, 4, 3);
             listBoxOilByStockCost.Name = "listBoxOilByStockCost";
-            listBoxOilByStockCost.Size = new Size(150, 95);
+            listBoxOilByStockCost.Size = new Size(174, 109);
             listBoxOilByStockCost.TabIndex = 14;
-
+            // 
             // btnRandomFill
-            btnRandomFill.Location = new Point(12, 381);
+            // 
+            btnRandomFill.Location = new Point(14, 440);
+            btnRandomFill.Margin = new Padding(4, 3, 4, 3);
             btnRandomFill.Name = "btnRandomFill";
-            btnRandomFill.Size = new Size(150, 30);
+            btnRandomFill.Size = new Size(175, 35);
             btnRandomFill.TabIndex = 15;
             btnRandomFill.Text = "Случайное заполнение";
             btnRandomFill.UseVisualStyleBackColor = true;
             btnRandomFill.Click += btnRandomFill_Click;
-
+            // 
             // btnImport
-            btnImport.Location = new Point(168, 381);
+            // 
+            btnImport.Location = new Point(196, 440);
+            btnImport.Margin = new Padding(4, 3, 4, 3);
             btnImport.Name = "btnImport";
-            btnImport.Size = new Size(150, 30);
+            btnImport.Size = new Size(175, 35);
             btnImport.TabIndex = 16;
             btnImport.Text = "Импорт из файла";
             btnImport.UseVisualStyleBackColor = true;
             btnImport.Click += btnImport_Click;
-
+            // 
             // btnExport
-            btnExport.Location = new Point(324, 381);
+            // 
+            btnExport.Location = new Point(378, 440);
+            btnExport.Margin = new Padding(4, 3, 4, 3);
             btnExport.Name = "btnExport";
-            btnExport.Size = new Size(150, 30);
+            btnExport.Size = new Size(175, 35);
             btnExport.TabIndex = 17;
             btnExport.Text = "Экспорт в файл";
             btnExport.UseVisualStyleBackColor = true;
             btnExport.Click += btnExport_Click;
-
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { импортToolStripMenuItem, экспортToolStripMenuItem, рассчитатьToolStripMenuItem, случайноеЗаполнениеToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(915, 24);
+            menuStrip1.TabIndex = 18;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // импортToolStripMenuItem
+            // 
+            импортToolStripMenuItem.Name = "импортToolStripMenuItem";
+            импортToolStripMenuItem.Size = new Size(63, 20);
+            импортToolStripMenuItem.Text = "Импорт";
+            импортToolStripMenuItem.Click += btnImport_Click;
+            // 
+            // экспортToolStripMenuItem
+            // 
+            экспортToolStripMenuItem.Name = "экспортToolStripMenuItem";
+            экспортToolStripMenuItem.Size = new Size(64, 20);
+            экспортToolStripMenuItem.Text = "Экспорт";
+            экспортToolStripMenuItem.Click += btnExport_Click;
+            // 
+            // рассчитатьToolStripMenuItem
+            // 
+            рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+            рассчитатьToolStripMenuItem.Size = new Size(80, 20);
+            рассчитатьToolStripMenuItem.Text = "Рассчитать";
+            рассчитатьToolStripMenuItem.Click += btnCalculate_Click;
+            // 
+            // случайноеЗаполнениеToolStripMenuItem
+            // 
+            случайноеЗаполнениеToolStripMenuItem.Name = "случайноеЗаполнениеToolStripMenuItem";
+            случайноеЗаполнениеToolStripMenuItem.Size = new Size(148, 20);
+            случайноеЗаполнениеToolStripMenuItem.Text = "Случайное заполнение";
+            случайноеЗаполнениеToolStripMenuItem.Click += btnRandomFill_Click;
+            // 
             // Form1
-            AutoScaleDimensions = new SizeF(6F, 13F);
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 421);
+            ClientSize = new Size(915, 486);
             Controls.Add(btnExport);
             Controls.Add(btnImport);
             Controls.Add(btnRandomFill);
@@ -204,11 +285,16 @@ namespace kursfordim
             Controls.Add(label1);
             Controls.Add(dataGridViewOils);
             Controls.Add(dataGridViewMachines);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
             Name = "Form1";
             Text = "Учет технических масел для станков";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)dataGridViewMachines).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridViewOils).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -231,5 +317,10 @@ namespace kursfordim
         private Button btnRandomFill;
         private Button btnImport;
         private Button btnExport;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem импортToolStripMenuItem;
+        private ToolStripMenuItem экспортToolStripMenuItem;
+        private ToolStripMenuItem рассчитатьToolStripMenuItem;
+        private ToolStripMenuItem случайноеЗаполнениеToolStripMenuItem;
     }
 }
